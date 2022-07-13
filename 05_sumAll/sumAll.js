e749ea3f8b7b0b4cafc779b45bfb7a07db739c49
rburaksaritas <rburaksaritas@gmail.com>
2022-07-13 20:57:41 +0300

    Add solution for exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (typeof min != "number" || typeof max != "number" || min<0 || max<0){
+        return "ERROR";
+    }
+    
+    let sum = 0;
+    
+    if (min>max){
+        let temp = min;
+        min = max;
+        max = temp;
+    }
 
+    for (let i=min; i<max+1; i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
