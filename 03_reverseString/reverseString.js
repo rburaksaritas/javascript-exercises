b6355a0724431beaac28fd184f7e50e82d863063
rburaksaritas <rburaksaritas@gmail.com>
2022-07-13 19:35:59 +0300

    Add solution for exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = "";
+    let length = string.length;
+    for(let i = length-1; i>-1; i--){
+        result += string.charAt(i);
+    }
+    return result;
 };
 
 // Do not edit below this line
