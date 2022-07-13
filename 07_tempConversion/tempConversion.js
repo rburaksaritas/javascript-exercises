ed4b1b321e89f5662c6b5df608ca7c4534b2cbd5
rburaksaritas <rburaksaritas@gmail.com>
2022-07-13 21:12:59 +0300

    Add solution for exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(tempInF) {
+  return Math.round(((tempInF - 32)*5/9)*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(tempInC) {
+  return Math.round(((tempInC*9/5)+32)*10)/10;
 };
 
 // Do not edit below this line
