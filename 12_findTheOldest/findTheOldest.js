095b793f3931d8aa5c6dc84b7555194ffcb0e107
rburaksaritas <rburaksaritas@gmail.com>
2022-07-25 20:48:25 +0300

    Add solution for exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear();
+    return people.sort(function(a,b){
+        if (!a.yearOfDeath){
+            a.yearOfDeath = currentYear;
+        } if (!b.yearOfDeath){
+            b.yearOfDeath = currentYear;
+        }
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        console.log(a.name + ": " + lastGuy + ", " + b.name + ": " + nextGuy);
+        return lastGuy > nextGuy ? -1 : 1;
+      })[0];
 };
 
 // Do not edit below this line
