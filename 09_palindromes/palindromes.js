910a06d5a33a6c02e35c90601eb7e340b9aa54ca
rburaksaritas <rburaksaritas@gmail.com>
2022-07-25 20:07:47 +0300

    Add solutions for exercises 9, 10, 11

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    let breaks = [" ", ",", ".", "!", "?", ";", ":"];
+    for(let i = 0; i<breaks.length; i++){
+        string = string.replaceAll(breaks[i], "");
+    }
+    let lettersToCheck = Math.floor(string.length/2);
+    let lastIndex = string.length-1;
+    for(let i = 0; i<lettersToCheck; i++){
+        if(string[i]!=string[lastIndex-i]){
+            return false;
+        }
+    }  return true;
 };
 
 // Do not edit below this line
